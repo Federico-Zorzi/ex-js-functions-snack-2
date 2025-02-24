e8fb559c213e9400c33adcb69f06690bbe443c60
Federico-Zorzi <federico.zorzi05@gmail.com>
2025-02-24 10:47:27 +0100

    snack 1

--- snack-1/script.js
+++ snack-1/script.js
@@ -6,3 +6,14 @@ Crea una funzione che somma due numeri.
     Poi, definisci la stessa funzione somma ma come funzione anonima assegnata a una variabile
     Quindi, riscrivi la funzione somma con la sintassi delle arrow functions.
 */
+const numero1 = 4;
+const numero2 = 5;
+
+function somma(num1, num2) {
+  return num1 + num2;
+}
+
+const anonimusSomma = (num1, num2) => num1 + num2;
+
+console.log(`${numero1} + ${numero2} = ${somma(numero1, numero2)}`);
+console.log(`${numero1} + ${numero2} = ${anonimusSomma(numero1, numero2)}`);
