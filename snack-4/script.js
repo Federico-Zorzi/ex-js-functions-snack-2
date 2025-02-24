d5aee1cf84307ac7b36df2c9b18c6cc931e012c2
Federico-Zorzi <federico.zorzi05@gmail.com>
2025-02-24 11:06:43 +0100

    snack 4

--- snack-4/script.js
+++ snack-4/script.js
@@ -4,3 +4,13 @@ Crea un generatore di funzioni creaTimer
 
     Scrivi una funzione creaTimer che accetta un tempo (in ms) e restituisce una nuova funzione che avvia un setTimeout per stampare "Tempo scaduto!".
 */
+
+function creaTimer(ms) {
+  return setTimeout(() => console.log(`Tempo scaduto: ${ms}ms`), ms);
+}
+
+console.log(creaTimer(1000));
+console.log(creaTimer(3000));
+console.log(creaTimer(5000));
+console.log(creaTimer(7000));
+console.log(creaTimer(9000));
