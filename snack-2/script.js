c359fa56052a38e576d66b3dee62660a7e5a727f
Federico-Zorzi <federico.zorzi05@gmail.com>
2025-02-24 10:52:50 +0100

    snack 2

--- snack-2/script.js
+++ snack-2/script.js
@@ -5,3 +5,8 @@ Crea una arrow function che calcola il quadrato di un numero.
     Definisci una funzione chiamata quadrato che accetta un numero e restituisce il suo quadrato in una sola riga.
 
  */
+const numero = 6;
+
+const quadrato = (num) => num * num;
+
+console.log(`${numero} * ${numero} = ${quadrato(numero)}`);
