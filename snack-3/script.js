babe3aff78fb95b77bb11716a46710b8eb76b367
Federico-Zorzi <federico.zorzi05@gmail.com>
2025-02-24 11:00:26 +0100

    snack 3

--- snack-3/script.js
+++ snack-3/script.js
@@ -13,3 +13,32 @@ Crea una funzione eseguiOperazione
         console.log(eseguiOperazione(3, 4, somma)); // 7
         console.log(eseguiOperazione(3, 4, moltiplica)); // 12
 */
+
+const numero1 = 15;
+const numero2 = 3;
+
+function eseguiOperazione(num1, num2, mathOperation) {
+  return mathOperation(num1, num2);
+}
+
+const addizione = (n1, n2) => n1 + n2;
+const sottrazione = (n1, n2) => n1 - n2;
+const moltiplicazione = (n1, n2) => n1 * n2;
+const divisione = (n1, n2) => n1 / n2;
+
+console.log(
+  `${numero1} + ${numero2} = ${eseguiOperazione(numero1, numero2, addizione)}`
+);
+console.log(
+  `${numero1} - ${numero2} = ${eseguiOperazione(numero1, numero2, sottrazione)}`
+);
+console.log(
+  `${numero1} * ${numero2} = ${eseguiOperazione(
+    numero1,
+    numero2,
+    moltiplicazione
+  )}`
+);
+console.log(
+  `${numero1} / ${numero2} = ${eseguiOperazione(numero1, numero2, divisione)}`
+);
